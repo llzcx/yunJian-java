1e835e9e6ff8b02d43b68bd99a48ee60c19de600
周威宇 <ouoow88888@hotmail.com>
2023-08-14 16:50:55 +0800

    fix: 简历分析扩展功能

---
+++ yunJian-common/src/main/java/ccw/ruan/common/constant/EducationType.java
@@ -0,0 +1,41 @@
+package ccw.ruan.common.constant;
+
+import lombok.Getter;
+
+/**
+ * @author 周威宇
+ */
+@Getter
+public enum EducationType {
+    /**
+     * 高中
+     */
+    HIGH_SCHOOL(1,"高中"),
+    /**
+     * 大专
+     */
+    JUNIOR_COLLEGE(2,"大专"),
+    /**
+     * 本科
+     */
+    UNDERGRADUATE_COURSE(3,"本科"),
+    /**
+     * 硕士
+     */
+    MASTER(4,"硕士"),
+    /**
+     * 博士
+     */
+    DOCTOR(5,"博士"),
+    ;
+
+
+    private Integer code;
+    private String message;
+
+
+    EducationType(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}

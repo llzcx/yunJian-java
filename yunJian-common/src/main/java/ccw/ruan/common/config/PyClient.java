a3090467a6d38ab330f5cf00c24163e65f103d63
cx <3207242935@qq.com>
2023-08-18 00:36:56 +0800

    fix: ip

--- yunJian-common/src/main/java/ccw/ruan/common/config/PyClient.java
+++ yunJian-common/src/main/java/ccw/ruan/common/config/PyClient.java
@@ -1,10 +1,21 @@
 package ccw.ruan.common.config;
 
 /**
+ * R9000P phone：
+ * 机房：
+ * R9000P:10.100.174.171
+ * 轻薄本：10.100.167.8
+ * ZWY：10.100.15.166
+ *
+ * 热点：
+ * R9000P:192.168.18.89
+ * 轻薄本：192.168.18.9
+ * ZWY：192.168.18.83
  * @author 陈翔
  */
 public interface PyClient {
-    String R9000P = "10.100.174.171:7070";
-    String ZWY = "http://10.100.15.166:7070";
-    String ZWY1 = "http://10.100.15.166:7071";
+    String R9000P = "192.168.18.89:7070";
+    String ZWY_IP = "192.168.18.83";
+    String ZWY = "http://"+ZWY_IP+":7070";
+    String ZWY1 = "http://"+ZWY_IP+":7071";
 }

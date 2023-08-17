83dab7d44ae51fdc381235add8da082b92ee9cf9
周威宇 <ouoow88888@hotmail.com>
2023-08-18 00:27:42 +0800

    fix: resume

--- yunJian-common/src/main/java/ccw/ruan/common/config/PyClient.java
+++ yunJian-common/src/main/java/ccw/ruan/common/config/PyClient.java
@@ -4,7 +4,9 @@ package ccw.ruan.common.config;
  * @author 陈翔
  */
 public interface PyClient {
-    String R9000P = "10.100.174.171:7070";
-    String ZWY = "http://10.100.15.166:7070";
-    String ZWY1 = "http://10.100.15.166:7071";
+    String R9000P = "192.168.18.89:7070";
+//    String R9000P = "10.100.174.171:7070";
+    //p2 = localhost
+    String ZWY = "http://localhost:7070";
+    String ZWY1 = "http://localhost:7071";
 }
